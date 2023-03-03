0b461bda37617fbbe68d5b9525eb6093b6d80237
Gutierrez Rafael <111995483+rafawga@users.noreply.github.com>
2023-03-03 20:14:53 -0300

    Add files via upload

---
+++ ConsoleApp1/Class1.cs
@@ -0,0 +1,16 @@
+﻿public static class Calculadora
+{
+
+
+    public static int Somar(int Valor1, int Valor2) => Valor1 + Valor2;
+
+    public static int Subtrair(int Valor1, int Valor2) => Valor1 - Valor2;
+
+    public static int Dividir(int Valor1, int Valor2) => Valor1 / Valor2;
+
+    public static int Multiplicar(int Valor1, int Valor2) => Valor1 * Valor2;
+
+
+
+
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,39 @@
+﻿
+Console.WriteLine(Calculadora.Somar(1,1));
+
+
+
+
+//Carro carro = new Carro("Civic", "Preto", 98);
+//carro.Mostrar();
+//carro.AlterAno(90);
+//carro.Mostrar();
+
+
+//public class Carro
+//{
+//     string Marca { get;  set; }
+//     string Cor { get; set; }
+//    int Ano { get; set; }
+
+
+//    public Carro(string marca, string cor, int ano)
+//    {
+//        this.Marca = marca;
+//        this.Cor = cor;
+//        this.Ano = ano;
+//    }
+
+
+//    public void Mostrar() => Console.WriteLine($"Meu carro é um {this.Marca} {this.Cor}, ano {this.Ano}");
+
+
+//    public void AlterAno(int ano) => this.Ano = ano;   
+
+//}
+
+
+
+
+
+
